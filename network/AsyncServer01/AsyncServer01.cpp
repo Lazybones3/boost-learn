b1d248bf18debfce07ad5f251035475658734ddf
Lazybones3 <caorui452@163.com>
2024-05-28 23:28:23 +0800

    update network

---
+++ network/AsyncServer01/AsyncServer01.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <boost/asio.hpp>
+#include "Session.h"
+int main()
+{
+	try {
+		boost::asio::io_context ioc;
+		using namespace std;
+		Server s(ioc, 10086);
+		ioc.run();
+	}
+	catch (std::exception& e) {
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+	return 0;
+}
+
---
+++ network/README.md
@@ -0,0 +1,13 @@
+同步服务：SyncServer、SyncClient
+
+应答模式的异步服务：AsyncServer01
+
+全双工通信模式以及缓冲队列：AsyncServer02
+
+处理粘包方式一：AsyncServer03、SyncClient03
+
+处理粘包方式二：ReadAtLeastServer
+
+粘包测试：PacketStickServer、PacketStickClient
+
+处理大小端字节序：
